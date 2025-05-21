5128ab9768a8096d828f72634e5c43b6cac9e929
Yuliia Hubska <zhukapuka10@gmail.com>
2025-05-21 20:12:21 +0300

    form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,27 @@
 export default function Form() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const form = event.currentTarget;
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const form = event.currentTarget;
 
-    const formData = new FormData(form);
-    const username = formData.get("username");
-    console.log("Username:", username);
+  //   const formData = new FormData(form);
+  //   const username = formData.get("username");
+  //   console.log("Username:", username);
 
-    form.reset();
+  //   form.reset();
+  // };
+
+  const handleSubmit = (formData: FormData) => {
+    const username = formData.get("username") as string;
+    console.log("Name:", username);
   };
+  // return (
+  //   <form onSubmit={handleSubmit}>
+  //     <input type="text" name="username" />
+  //     <button type="submit">Submit</button>
+  //   </form>
+  // );
   return (
-    <form onSubmit={handleSubmit}>
+    <form action={handleSubmit}>
       <input type="text" name="username" />
       <button type="submit">Submit</button>
     </form>
