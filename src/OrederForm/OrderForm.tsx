7c947852678d22f3042954546aa5e2595e4563ad
Yuliia Hubska <zhukapuka10@gmail.com>
2025-05-23 13:39:22 +0300

    add

--- src/OrederForm/OrderForm.tsx
+++ src/OrederForm/OrderForm.tsx
@@ -15,4 +15,19 @@ export default function OrderForm() {
       <button type="submit">Place order</button>
     </form>
   );
+  // Один id для групи полів
+  // return (
+  //   <form>
+  //     <label htmlFor={`${fieldId}-name`}>Name</label>
+  //     <input type="text" name="username" id={`${fieldId}-username`} />
+
+  //     <label htmlFor={`${fieldId}-email`}>Email</label>
+  //     <input type="email" name="email" id={`${fieldId}-email`} />
+
+  //     <button type="submit">Place order</button>
+  //   </form>
+  // );
+  //     Ми створили один унікальний fieldId за допомогою useId()
+  // Додали до нього суфікси username і email для створення окремих ідентифікаторів для кожного поля
+  // Це зменшує кількість викликів useId() і полегшує читання коду
 }
